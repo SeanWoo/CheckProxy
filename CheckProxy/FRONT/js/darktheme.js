fa7bcec6f4073c86ecb9a00a27c6e0610a4a2681
kgbsenior <Rostik3014@gmail.com>
2020-10-13 16:37:31 +0300

    refactor started!

--- CheckProxy/FRONT/js/darktheme.js
+++ CheckProxy/FRONT/js/darktheme.js
@@ -1,18 +1,17 @@
-let themeswitcher = document.getElementById('theme-switcher')
-let dark = readCookie('dark')
-console.log(dark+typeof(dark))
+let themeswitcher = document.getElementById('theme-switcher');
+let isDark = (readCookie('isDark') == 'true');
 
-if(dark=='true'){
-    document.body.classList.add("darkbody")
-    themeswitcher.querySelector('i').className = "fas fa-sun"
-    dark = 'true'
-    createCookie('dark', 'true', '10')
+if(isDark){
+    document.body.classList.add("darkbody");
+    themeswitcher.querySelector('i').className = "fas fa-sun";
+    isDark = !isDark;
+    createCookie('isDark', true, '10');
 }
-else if(dark=='false'){
-    document.body.classList.remove("darkbody")
-    themeswitcher.querySelector('i').className = "fas fa-moon"
-    dark = 'false'
-    createCookie('dark', 'false', '10')
+else if(!isDark){
+    document.body.classList.remove("darkbody");
+    themeswitcher.querySelector('i').className = "fas fa-moon";
+    isDark = !isDark;
+    createCookie('isDark', false, '10');
 }
 
 function createCookie(name,value,days) {
@@ -28,37 +27,36 @@ function createCookie(name,value,days) {
 function readCookie(name) {
     var nameEQ = name + "=";
     var ca = document.cookie.split(';');
-    for(var i=0;i < ca.length;i++) {
+    for(var i=0; i < ca.length; i++) {
         var c = ca[i];
-        while (c.charAt(0)==' ') c = c.substring(1,c.length);
-        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+        while (c.charAt(0)==' ') c = c.substring(1, c.length);
+        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
     }
     return null;
 }
 
 themeswitcher.addEventListener('click', ()=>{
-    switchtheme()
+    switchtheme();
 })
 
 function switchtheme(){
-    console.log(dark+typeof(dark))
-    if(dark=='true'){
-        document.body.classList.remove("darkbody")
-        themeswitcher.querySelector('i').className = "fas fa-moon"
-        dark = 'false'
-        createCookie('dark', 'false', '10')
+    if(isDark){
+        document.body.classList.remove("darkbody");
+        themeswitcher.querySelector('i').className = "fas fa-moon";
+        isDark = !isDark;
+        createCookie('isDark', false, '10');
     }
-    else if(dark=='false'){
-        document.body.classList.add("darkbody")
-        themeswitcher.querySelector('i').className = "fas fa-sun"
-        dark = 'true'
-        createCookie('dark', 'true', '10')
+    else if(!isDark){
+        document.body.classList.add("darkbody");
+        themeswitcher.querySelector('i').className = "fas fa-sun";
+        isDark = !isDark;
+        createCookie('isDark', true, '10');
     }
-    else if(dark!='false'&dark!='true'){
-        document.body.classList.add("darkbody")
-        themeswitcher.querySelector('i').className = "fas fa-sun"
-        dark = 'true'
-        createCookie('dark', 'true', '10')
+    else if(isDark!=false&isDark!=true){
+        document.body.classList.add("darkbody");
+        themeswitcher.querySelector('i').className = "fas fa-sun";
+        isDark = !isDark;
+        createCookie('isDark', true, '10');
     }
 }
 
