b5e4f6aaa96f8cc9f319f346eb9f860acc25bbc3
SeanWoo <kokarevvadim2002@gmail.com>
2020-10-20 18:33:51 +0400

    small code

--- CheckProxy/Pages/CheckPort.cshtml.cs
+++ CheckProxy/Pages/CheckPort.cshtml.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CheckProxy.Services.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -9,8 +10,26 @@ namespace CheckProxy.Pages
 {
     public class CheckPortModel : PageModel
     {
-        public void OnGet()
+        private ICore _core;
+
+        private string _ip;
+        private List<int> _ports;
+
+        [BindProperty]
+        public string IP { get; set; }
+        [BindProperty]
+        public string Ports { get; set; }
+
+        public CheckPortModel(ICore core) => _core = core;
+
+        public void OnGetAsync()
         {
+
+        }
+
+        public void OnPostAsync()
+        {
+            
         }
     }
 }
--- CheckProxy/Pages/CheckProxy.cshtml.cs
+++ CheckProxy/Pages/CheckProxy.cshtml.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CheckProxy.Services.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -9,8 +10,22 @@ namespace CheckProxy.Pages
 {
     public class CheckProxyModel : PageModel
     {
-        public void OnGet()
+        private ICore _core;
+        private List<string[]> _proxys;
+
+        [BindProperty]
+        public string Proxys { get; set; }
+
+        public CheckProxyModel(ICore core) => _core = core;
+        public void OnGetAsync()
         {
+
+        }
+
+        public void OnPostAsync()
+        {
+            _proxys = Proxys.Split('\n').Select(x => x.Split(' ')).ToList();
+
         }
     }
 }
